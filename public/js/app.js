7fa83627cb04fa99750da3749ea3c98226c2215a
KAINOA KEANAAINA <Kainoa@KAINOAs-MacBook-Air.local>
2015-12-23 19:33:51 -1000

    random buttom incomplete

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ window.onload = function(){
 
   $.getJSON('./api/get_the_app.json',function(data){     //first argument -path to our data. //second argument - call back function()
 
-
+    console.log(data.data.children.length);
 
     for(var i=0; i<data.data.children.length; i++){
     var div = document.createElement('div');
@@ -31,52 +31,64 @@ window.onload = function(){
   });
 
 
+  //this array is to save the random numbers
+  var arrayOfRandoms = [];
 
+  console.log(arrayOfRandoms.length);
+var alohaloha = 50;
 
-
-
+  //this won't work unless it's in the get.JSON
+  var makingArrayofRandoms = function(){
+  // this won't work unless it's in the get.JSON
+    var contentLength = alohaloha;
+    while(arrayOfRandoms.length===contentLength){
+      var randomNum = Math.floor(Math.random()*contentLength);
+      if(arrayOfRandoms.indexOf(randomNum)=== -1){
+       arrayOfRandoms.push(randomNum);
+      } //if
+    } //while loop
+  };
 
 
 
   var randomButton = document.getElementById('randomButton');
   randomButton.addEventListener('click',function(){
 
-  //clear the blockContent
-  var contentLength = document.getElementsByClassName('divContent').length;
+    //this resets the array where the random numbers are saved
+    arrayOfRandoms=[];
 
-    for(var i = 0 ; i < contentLength ; i++){
+    //this erase all divs in the divContent
+    for(var i = 0 ; i < alohaloha ; i++){
       document.getElementsByClassName('block-content')[0].removeChild(document.getElementById('id'+i));
     }
 
-//document.getElementsByClassName('block-content')[0].removeChild(document.getElementById('id0'))
-    //reenter the blockContent with divs at a random count
-    //math random
-    var arrayOfRandoms = [];
-    for(var j = 0; j< contentLength ; j++){
-      var randomNum = Math.floor(Math.random()*contentLength);
-      if(arrayOfRandoms.indexOf(randomNum)=== -1){
-        arrayOfRandoms.push(randomNum);
+    $.getJSON('./api/get_the_app.json',function(data){     //first argument -path to our data. //second argument - call back function()
+
+      //this pushes the random numbers in the array
+      makingArrayofRandoms();
+
+      console.log(data.data.children.length);
+
+
+      for(var i=0; i<data.data.children.length; i++){
+      var div = document.createElement('div');
+      div.className = 'divContent';
+      div.id = 'id'+i;
+
+      document.getElementsByClassName('block-content')[0].appendChild(div);
+      var image = data.data.children[arrayOfRandoms[i]].data.url;
+      var title = data.data.children[arrayOfRandoms[i]].data.title;
+      var author = data.data.children[arrayOfRandoms[i]].data.author;
+
+
+
+      $(div).append('<img src = " '+image+'"></img>');
+      $(div).append('<h3>'+title+'</div>');
+      $(div).append('<h5> by '+author+'</h5>');
+
       }
-      console.log(arrayOfRandoms);
 
-      var indexNum = arrayOfRandoms[j];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    } //for loop
+  });
 
 
 });
