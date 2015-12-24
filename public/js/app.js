ff85ee256fa928e651fd93d8db43d702b53c276f
KAINOA KEANAAINA <Kainoa@KAINOAs-MacBook-Air.local>
2015-12-23 17:02:36 -1000

    random button clears the block-content

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-var divv = document.getElementsByClassName('block-content')[0];
-
 window.onload = function(){
   // do stuff
   var header = document.createElement('img');
@@ -10,15 +8,18 @@ window.onload = function(){
 
   $.getJSON('./api/get_the_app.json',function(data){     //first argument -path to our data. //second argument - call back function()
 
-    console.log(data);
+
 
     for(var i=0; i<data.data.children.length; i++){
     var div = document.createElement('div');
     div.className = 'divContent';
+    div.id = 'id'+i;
+
     document.getElementsByClassName('block-content')[0].appendChild(div);
     var image = data.data.children[i].data.url;
     var title = data.data.children[i].data.title;
     var author = data.data.children[i].data.author;
+
 
 
     $(div).append('<img src = " '+image+'"></img>');
@@ -28,5 +29,66 @@ window.onload = function(){
     }
 
   });
+
+
+
+
+
+
+
+
+
+  var randomButton = document.getElementById('randomButton');
+  randomButton.addEventListener('click',function(){
+
+  //clear the blockContent
+  var contentLength = document.getElementsByClassName('divContent').length;
+
+    for(var i = 0 ; i < contentLength ; i++){
+      document.getElementsByClassName('block-content')[0].removeChild(document.getElementById('id'+i));
+    }
+
+//document.getElementsByClassName('block-content')[0].removeChild(document.getElementById('id0'))
+    //reenter the blockContent with divs at a random count
+    //math random
+    var arrayOfRandoms = [];
+    for(var j = 0; j< contentLength ; j++){
+      var randomNum = Math.floor(Math.random()*contentLength);
+      if(arrayOfRandoms.indexOf(randomNum)=== -1){
+        arrayOfRandoms.push(randomNum);
+      }
+      console.log(arrayOfRandoms);
+
+      var indexNum = arrayOfRandoms[j];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } //for loop
+
+
+});
+
+
+
+
+
+
+
+
+
+
 };
 
